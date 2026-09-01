b03994a300238b282462f9a7a4ab412cba07eb82
Luix13 <nanaluix@gmail.com>
2026-09-02 00:36:02 +0300

    new files echo

--- src/echo.py
+++ src/echo.py
@@ -3,6 +3,13 @@ import requests
 
 def get_echo_data():
     url = "https://postman-echo.com/get"
-
     response = requests.get(url, timeout=10)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_echo_with_params(params):
+    url = "https://postman-echo.com/get"
+    response = requests.get(url, params=params, timeout=10)
+    response.raise_for_status()
     return response.json()
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,8 +1,17 @@
+import pytest
 
-from src.echo import get_echo_data
+from src.echo import get_echo_data, get_echo_with_params
 
-
+@pytest.mark.get
 def test_get_none_param():
     data = get_echo_data()
     assert "headers" in data
 
+@pytest.mark.get_params
+def test_parameters_are_sent():
+    sent_data = {"name": "Alice", "age": 30, "city": "Moscow"} # Числа остаются числами!
+    response = get_echo_with_params(sent_data)
+
+    assert "name" in response["args"]
+    assert "age" in response["args"]
+    assert "city" in response["args"]
